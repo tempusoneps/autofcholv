caf7567303e4b2f4aa06a4a81895eecc72405fc4
Zuongthao <zuongthao.tn@gmail.com>
2026-03-17 06:47:07 +0700

    create pipeline

--- src/autofcholv/core.py
+++ src/autofcholv/core.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from autofcholv.pipeline.validating import validate_ohlcv_dataset
+from autofcholv.pipeline.validation import validate_ohlcv_dataset
 from autofcholv.pipeline.cleaning import clean_ohlcv
-from autofcholv.pipeline.feature_engineering import feature_engineering
-from autofcholv.pipeline.feature_preprocessing import feature_preprocessing
+from autofcholv.pipeline.feature_engineering import build_features
+from autofcholv.pipeline.preprocessing import preprocess_data
 
 def extract_features(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -13,11 +13,17 @@ def extract_features(df: pd.DataFrame) -> pd.DataFrame:
     """
     
     # do step 1: validate data
+    is_valid, error_details = validate_ohlcv_dataset(df)
+    if not is_valid:
+        raise ValueError(f"Invalid OHLCV data: {error_details}")
 
     # do step 2: Clean data
+    df = clean_ohlcv(df)
 
     # do step 3: feature engineering
+    df = build_features(df)
 
     # do step 4: feature preprocessing
+    df = preprocess_data(df)
         
-    return features
+    return df
