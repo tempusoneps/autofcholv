52dda53ed83590d44ce18c8b37deea2b30457650
Zuongthao <zuongthao.tn@gmail.com>
2026-08-11 06:13:46 +0700

    modify time.py

--- src/autofcholv/pipeline/features/time.py
+++ src/autofcholv/pipeline/features/time.py
@@ -19,14 +19,8 @@ def extract_features(df: pd.DataFrame, _config: Config) -> pd.DataFrame:
     df['year'] = df.index.year
     df['day_of_week'] = df.index.dayofweek
     df['time_int'] = df['hour'] * 100 + df['minute']
-    df["trade_date"] = df.index.normalize()
-    df["bar_in_day"] = df.groupby("trade_date").cumcount()
-    df["session_0930_1335"] = (df["time_int"] >= 930) & (df["time_int"] <= 1335)
-    df["session_0935_1425"] = (df["time_int"] >= 935) & (df["time_int"] < 1425)
-    df["session_0935_1335"] = (df["time_int"] >= 935) & (df["time_int"] <= 1335)
-    df["late_session_1325"] = df["time_int"].isin({1325, 1340, 1355})
-    df["late_session_1310"] = df["time_int"].isin({1310, 1325, 1340, 1355})
-    df["entry_window_1300_1425"] = (df["time_int"] >= 1300) & (df["time_int"] <= 1425)
     df["session_progress"] = ((df.hour * 60 + df.minute) - 9 * 60) / (51 * 5)
+    df["is_morning"] = (df["time_int"] < 1200)
+    df["is_afternoon"] = (df["time_int"] > 1200)
 
     return df
